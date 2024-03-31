9a10679f911037349113f3952311ffab325b3de9
Sina Hosseini <synahsyny042@gmail.com>
2024-03-31 16:22:19 +0330

    Update x-ui-backup.sh

--- x-ui-backup.sh
+++ x-ui-backup.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
 
-# Function to display error messages
-display_error() {
-    echo "Error: $1"
-    exit 1
-}
+# Define message to be sent along with the file
+MESSAGE=""
 
 # Get Discord webhook URL from user
 read -p "Please enter the Discord webhook URL: " WEBHOOK_URL
-[[ -z "$WEBHOOK_URL" ]] && display_error "Discord webhook URL cannot be empty."
 
 # Get message from user
 read -p "Please enter the message: " MESSAGE
 
+# Install zip package
+sudo apt update
+sudo apt install zip -y
+
 # Remove existing backup zip file
-rm -rf /root/SinaBigSmoke-x.zip
+sudo rm -rf /root/SinaBigSmoke-x.zip
 
 # Create a zip file containing specified files
-zip /root/SinaBigSmoke-x.zip /etc/x-ui/x-ui.db /usr/local/x-ui/config.json || display_error "Failed to create backup zip file."
+sudo zip /root/SinaBigSmoke-x.zip /etc/x-ui/x-ui.db /usr/local/x-ui/config.json
 
 # Add a comment to the zip file
-echo -e "$MESSAGE" | zip -z /root/SinaBigSmoke-x.zip || display_error "Failed to add comment to backup zip file."
+echo -e "$MESSAGE" | sudo zip -z /root/SinaBigSmoke-x.zip
 
 # Define path to the zip file
 FILE_PATH="/root/SinaBigSmoke-x.zip"
 
 # Add the cron job
-(crontab -l 2>/dev/null; echo "0 0 * * * /bin/bash /root/backup_script.sh") | crontab - || display_error "Failed to add cron job."
+echo "0 0 * * * /bin/bash /root/backup_script.sh" | sudo crontab -
 
 # Send the file using curl
-curl_output=$(curl -s -X POST -H "Content-Type: multipart/form-data" -F "content=$MESSAGE" -F "file=@$FILE_PATH" "$WEBHOOK_URL")
-[[ "$curl_output" != "ok" ]] && display_error "Failed to send backup file to Discord webhook."
+curl -X POST -H "Content-Type: multipart/form-data" -F "content=$MESSAGE" -F "file=@$FILE_PATH" $WEBHOOK_URL
 
 # Display success message
 echo "Backup file sent successfully to Discord webhook."
