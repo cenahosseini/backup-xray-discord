1187c6e803e681eebd3e8aaf1bb1c28b1c19659c
Sina Hosseini <synahsyny042@gmail.com>
2024-04-01 17:12:02 +0330

    Fixed bug

--- SinaBigSmoke_marzban.sh
+++ SinaBigSmoke_marzban.sh
@@ -13,33 +13,59 @@ read_input_from_config() {
     INPUT=$(cat "$1")
 }
 
-if [ ! -f "$CONFIG_FILE_WEBHOOK" ] || [ ! -f "$CONFIG_FILE_MESSAGE" ] || [ ! -f "$CONFIG_FILE_CRON" ]; then
-    get_input_and_save "Please enter the Discord webhook URL: " "$CONFIG_FILE_WEBHOOK"
-    get_input_and_save "Please enter the message: " "$CONFIG_FILE_MESSAGE"
-    get_input_and_save "Please enter the cron job schedule (e.g., '0 0 * * *' for daily at midnight): " "$CONFIG_FILE_CRON"
-fi
+# Function to check if configuration files exist and create them if not
+check_and_create_config_files() {
+    if [ ! -f "$CONFIG_FILE_WEBHOOK" ] || [ ! -f "$CONFIG_FILE_MESSAGE" ] || [ ! -f "$CONFIG_FILE_CRON" ]; then
+        get_input_and_save "Please enter the Discord webhook URL: " "$CONFIG_FILE_WEBHOOK"
+        get_input_and_save "Please enter the message: " "$CONFIG_FILE_MESSAGE"
+        get_input_and_save "Please enter the cron job schedule (e.g., '0 0 * * *' for daily at midnight): " "$CONFIG_FILE_CRON"
+    fi
+}
 
-read_input_from_config "$CONFIG_FILE_WEBHOOK"
-WEBHOOK_URL="$INPUT"
+# Read input from configuration files
+read_input_from_config_files() {
+    read_input_from_config "$CONFIG_FILE_WEBHOOK"
+    WEBHOOK_URL="$INPUT"
+    
+    read_input_from_config "$CONFIG_FILE_MESSAGE"
+    MESSAGE="$INPUT"
+    
+    read_input_from_config "$CONFIG_FILE_CRON"
+    CRON_JOB="$INPUT"
+}
 
-read_input_from_config "$CONFIG_FILE_MESSAGE"
-MESSAGE="$INPUT"
+# Check and create configuration files if necessary
+check_and_create_config_files
 
-read_input_from_config "$CONFIG_FILE_CRON"
-CRON_JOB="$INPUT"
+# Read input from configuration files
+read_input_from_config_files
 
+# Update system and install zip package
 sudo apt update
 sudo apt install zip -y
-sudo rm -rf /root/SinaBigSmoke-m.zip
-FILE_PATH="/root/SinaBigSmoke-m.zip"
+
+# Define file path for backup
+FILE_PATH="/opt/SinaBigSmoke-m.zip"
+
+# Remove existing backup zip file
+sudo rm -rf $FILE_PATH
+
+# Display current system time
 echo "Current system time:"
 date
+
+# Display chosen cron job
 echo "Chosen cron job schedule:"
 echo "$CRON_JOB"
+
+# Display existing cron jobs
 echo "Cron jobs:"
 sudo crontab -l
+
+# Add the cron job
 echo "$CRON_JOB /bin/bash /root/SinaBigSmoke_marzban.sh" | sudo crontab -
 
+# Check if marzban directory exists
 if dir=$(find /opt /root -type d -iname "marzban" -print -quit); then
     echo "The folder exists at $dir"
 else
@@ -47,6 +73,7 @@ else
     exit 1
 fi
 
+# Perform Marzban backup
 if [ -d "/var/lib/marzban/mysql" ]; then
     sed -i -e 's/\s*=\s*/=/' -e 's/\s*:\s*/:/' -e 's/^\s*//' /opt/marzban/.env
     docker exec marzban-mysql-1 bash -c "mkdir -p /var/lib/mysql/db-backup"
@@ -75,5 +102,11 @@ else
     ZIP="zip -r $FILE_PATH ${dir}/* /var/lib/marzban/* /opt/marzban/.env"
 fi
 
+# Execute ZIP commands
+eval "$ZIP"
+
+# Send the Marzban backup file using curl
 curl -X POST -H "Content-Type: multipart/form-data" -F "content=$MESSAGE" -F "file=@$FILE_PATH" $WEBHOOK_URL
+
+# Display success message
 echo "Backup marzban file sent successfully to Discord webhook. Coded By SinaBigSmoke <3"
